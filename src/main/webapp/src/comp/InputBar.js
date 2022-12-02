8993d6e9cf2c1280e4e146ae217732a54eacf854
grbm <olejnec@gmail.com>
2022-12-02 16:04:53 +0300

    basic

--- src/main/webapp/src/comp/InputBar.js
+++ src/main/webapp/src/comp/InputBar.js
@@ -9,28 +9,50 @@ const InputBar = () => {
     })
 
     const {title, text} = post
+    const [frontError, setFrontError] = useState(false)
+
+    const handleSubmit = (e) => {
+        if (title.length < 3 || text.length < 1) {
+            e.preventDefault()
+            setFrontError(true)
+        }
+        console.log(post)
+    }
 
     return (
-        <div className="container is-max-desktop">
-            <input className="input is-small"
-                   type="text"
-                   placeholder="Set title"
-                   name="title"
-                   value={title}
-                   onChange={(e) => setPost({...post, [e.target.name]: e.target.value})}
-            />
-            <textarea className="textarea is-small"
-                      placeholder="Set text"
-                      name="text"
-                      value={text}
-                      onChange={(e) => setPost({...post, [e.target.name]: e.target.value})}
-            />
-            <button className="button is-link"
-                    onClick={() => Service.addPost(post)}
-            >Create<
-            /button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="container is-max-desktop">
+                <div className="container mt-2">
+                <input className="input is-normal"
+                       type="text"
+                       placeholder="Set title"
+                       name="title"
+                       value={title}
+                       onChange={(e) => setPost({...post, [e.target.name]: e.target.value})}
+                />
+                {frontError && title.length < 3 ?
+                    <label>Title must be 3 chars or more</label> : ""
+                }
+                </div>
+                <div className="container mt-2">
+                <textarea className="textarea is-normal"
+                          placeholder="Set text"
+                          name="text"
+                          value={text}
+                          onChange={(e) => setPost({...post, [e.target.name]: e.target.value})}
+                />
+                {frontError && text.length < 1 ?
+                    <label>Text must be 1 char or more</label> : ""
+                }
+                </div>
+                <br/>
+                <button className="button is-link mt-auto"
+                        onClick={() => Service.addPost(post)}
+                >Create
+                </button>
+            </div>
+        </form>
     );
-    };
+};
 
 export default InputBar;
