0b519eea61b8b2ef15bc336d33ead8399cb05c63
fmist <olejnec@gmail.com>
2023-06-30 14:31:13 +0300

    30

--- src/test/java/autotest/RestTest.java
+++ src/test/java/autotest/RestTest.java
@@ -6,7 +6,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
